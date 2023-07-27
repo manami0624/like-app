a11c152c3b937d50429d17d53240124bae2851c2
manami0624 <manana624@yahoo.co.jp>
2023-07-27 16:35:12 +0900

    いいね機能の非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_tweet
+
   def create
     like = current_user.likes.build(tweet_id: params[:tweet_id])
     like.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
   end
 end

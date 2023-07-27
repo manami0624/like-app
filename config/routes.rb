5305da4911f8a06cbdfb10a942672580a8cba200
manami0624 <manana624@yahoo.co.jp>
2023-07-27 16:22:56 +0900

    コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets, only: [:index, :new, :create] do
-    resources :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
   resources :users, only: [:show]
 end
